24269fbbc5b6a397954c537da4f0d709c245fd1e
Tushar Prabhu <149789869+tusharprabhu26@users.noreply.github.com>
2024-02-20 13:09:39 +0530

    Initial commit

---
+++ gates/js-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+
+npx nyc --reporter=html --reporter=text npm run test
+npx nyc check-coverage --functions 100
+npx nyc check-coverage --branches 100
+npx nyc check-coverage --lines 100
+
+npx stryker run --mutate ./*.js,\!.eslintrc.js,\!stryker.conf.js
